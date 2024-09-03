032f31b024c15da703a796babeacf633c87b8cf2
SuporteAutomax <suporte@automax.net.br>
2024-09-03 20:57:04 -0300

    ajustes nos arquivos

---
+++ models/questions.js
@@ -0,0 +1,43 @@
+const mongoose = require('../database/db');
+
+const QuestionsModelSchema = new mongoose.Schema({
+  type:{
+    type: String,
+    required: true,
+  },
+  title:{
+    type: String,
+    required: true,
+  },
+  question:{
+    type: String,
+  },
+  answers: {
+    type: Array,
+    default: []
+  },
+  date: {
+    type: Date,
+    default: new Date()
+  },
+  user_name: {
+    type: String,
+    default: "Anonymous"
+  },
+  best_answer: {
+    type: Boolean,
+    default: false
+  },
+  closed: {
+    type: Boolean,
+    default: false
+  },
+  has_image: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const QuestionsModel = mongoose.model ('questions', QuestionsModelSchema);
+
+module.exports = QuestionsModel;
