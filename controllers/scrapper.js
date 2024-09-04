bf6518308335d1f1b6da752eb1203eaa4e949274
SuporteAutomax <suporte@automax.net.br>
2024-09-03 21:04:32 -0300

    configuração do bot para busca dos valores individuais

--- controllers/scrapper.js
+++ controllers/scrapper.js
@@ -5,9 +5,90 @@ const Scrapper = () => {}
 
 Scrapper.run = async function (pageNumber) {
     try {
-        const currentData = await QuestionsModel.find()
-        return (parseInt(pageNumber) + 2)
+        
+        return (parseInt(pageNumber) + 5)
     } catch (e) {
+        return null
+    }
+}
+
+Scrapper.getType = async function(page){
+    try {
+
+    } catch (e) {
+        console.log(e)
+        return null
+    }
+}
+
+Scrapper.getTitle = async function(page){
+    try {
+
+    } catch (e) {
+        console.log(e)
+        return null
+    }
+}
+
+Scrapper.getQuestion = async function(page){
+    try {
+
+    } catch (e) {
+        console.log(e)
+        return null
+    }
+}
+
+Scrapper.getAnswer = async function(page){
+    try {
+
+    } catch (e) {
+        console.log(e)
+        return null
+    }
+}
+
+Scrapper.getDate = async function(page){
+    try {
+
+    } catch (e) {
+        console.log(e)
+        return null
+    }
+}
+
+Scrapper.getUserName = async function(page){
+    try {
+
+    } catch (e) {
+        console.log(e)
+        return null
+    }
+}
+
+Scrapper.getBestAnswer = async function(page){
+    try {
+
+    } catch (e) {
+        console.log(e)
+        return null
+    }
+}
+
+Scrapper.getClosed = async function(page){
+    try {
+
+    } catch (e) {
+        console.log(e)
+        return null
+    }
+}
+
+Scrapper.getHasImage = async function(page){
+    try {
+
+    } catch (e) {
+        console.log(e)
         return null
     }
 }
