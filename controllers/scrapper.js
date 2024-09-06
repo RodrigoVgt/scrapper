f391250d652c55a06650b42406798069015f3616
SuporteAutomax <suporte@automax.net.br>
2024-09-06 16:56:17 -0300

    ajustes

--- controllers/scrapper.js
+++ controllers/scrapper.js
@@ -1,11 +1,51 @@
 const QuestionsModel = require('../models/questions')
-const Puppeter = require('puppeteer')
+const puppeteer = require('puppeteer')
 
 const Scrapper = () => {}
 
 Scrapper.run = async function (pageNumber) {
     try {
-        
+        let created = 0
+        let error = 0
+        const browser = await puppeteer.launch({args: ['--no-sandbox'], headless: false});
+        const page = await browser.newPage();
+
+        for(let i = 0; i < 5; i++){
+            try {
+                const searchPage = `https://oficinabrasil.com.br/forum?page=${i + parseInt(pageNumber)}`
+                page.goto(searchPage)
+                const type = await this.getType(searchPage)
+                const title = await this.getTitle(searchPage)
+                const question = await this.getQuestion(searchPage)
+                const answer = await this.getAnswer(searchPage)
+                const date = await this.getDate(searchPage)
+                const user_name = await this.getUserName(searchPage)
+                const best_answer = await this.getBestAnswer(searchPage)
+                const closed = await this.getClosed(searchPage)
+                const has_image = await this.getHasImage(searchPage)
+                
+                if(!question || !answer || answer.length < 1) {
+                    error++
+                    continue
+                }
+
+                const questionObj = {
+                    type, title, question, answer, date, user_name, best_answer, closed, has_image
+                }
+                const result = await this.new(questionObj)
+                if(result) {
+                    created++ 
+                    continue
+                }
+                error++
+            } catch (e) {
+                error++
+                console.log(e)
+                continue
+            }
+        }
+
+        console.log("criados: " + created, "Erros: " + error)
         return (parseInt(pageNumber) + 5)
     } catch (e) {
         return null
@@ -44,7 +84,7 @@ Scrapper.getAnswer = async function(page){
 
     } catch (e) {
         console.log(e)
-        return null
+        return []
     }
 }
 
@@ -53,7 +93,7 @@ Scrapper.getDate = async function(page){
 
     } catch (e) {
         console.log(e)
-        return null
+        return new Date()
     }
 }
 
@@ -62,7 +102,7 @@ Scrapper.getUserName = async function(page){
 
     } catch (e) {
         console.log(e)
-        return null
+        return "Sem user"
     }
 }
 
