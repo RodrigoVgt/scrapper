b2cd75848b88946079861451eebba11b710f236d
SuporteAutomax <suporte@automax.net.br>
2024-09-06 21:30:23 -0300

    XD

--- controllers/scrapper.js
+++ controllers/scrapper.js
@@ -7,6 +7,8 @@ Scrapper.run = async function (pageNumber) {
     try {
         let created = 0
         let error = 0
+        let pageError = 0
+        const questionObj = []
         const browser = await puppeteer.launch({args: ['--no-sandbox'], headless: false});
         const page = await browser.newPage();
 
@@ -14,38 +16,51 @@ Scrapper.run = async function (pageNumber) {
             try {
                 const searchPage = `https://oficinabrasil.com.br/forum?page=${i + parseInt(pageNumber)}`
                 page.goto(searchPage)
-                const type = await this.getType(searchPage)
-                const title = await this.getTitle(searchPage)
-                const question = await this.getQuestion(searchPage)
-                const answer = await this.getAnswer(searchPage)
-                const date = await this.getDate(searchPage)
-                const user_name = await this.getUserName(searchPage)
-                const best_answer = await this.getBestAnswer(searchPage)
-                const closed = await this.getClosed(searchPage)
-                const has_image = await this.getHasImage(searchPage)
-                
-                if(!question || !answer || answer.length < 1) {
-                    error++
-                    continue
-                }
 
-                const questionObj = {
-                    type, title, question, answer, date, user_name, best_answer, closed, has_image
+                await page.waitForSelector('div.relative.bottom-8.-mb-8.flex.justify-end.gap-2');
+
+
+                const currentQuestions = await page.$x('/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div[2]/div/div')
+
+                for(const iterator of currentQuestions){
+                    try{
+                        const type = await this.getType(iterator)
+                        const title = await this.getTitle(iterator)
+                        const question = await this.getQuestion(iterator)
+                        const date = await this.getDate(iterator)
+                        const user_name = await this.getUserName(iterator)
+                        const has_image = await this.getHasImage(iterator)
+                        const answer = await this.getAnswer(iterator)
+                        const best_answer = await this.getBestAnswer(iterator)
+                        const closed = await this.getClosed(iterator)
+                        if(!question || !answer || answer.length < 1) {
+                            error++
+                            continue
+                        }
+        
+                        questionObj.push({
+                            type, title, question, answer, date, user_name, best_answer, closed, has_image
+                        })
+                    } catch(e) {
+                        console.log(e)
+                        error++
+                        continue
+                    }
                 }
-                const result = await this.new(questionObj)
-                if(result) {
-                    created++ 
-                    continue
-                }
-                error++
             } catch (e) {
-                error++
+                pageError++
                 console.log(e)
                 continue
             }
         }
 
-        console.log("criados: " + created, "Erros: " + error)
+        questionObj.forEach(async (question) => {
+            const result = await this.new(question)
+            if(result) created++ 
+            else error++
+        })
+
+        console.log("criados: " + created, "Erros: " + error, "Erro de página: " + pageError)
         return (parseInt(pageNumber) + 5)
     } catch (e) {
         return null
@@ -54,7 +69,8 @@ Scrapper.run = async function (pageNumber) {
 
 Scrapper.getType = async function(page){
     try {
-
+        const superiorText = await page.$eval('div div div.relative.bottom-8.-mb-8.flex.justify-end', element => element.innerText)
+        return superiorText ? superiorText.trim().toLowerCase() : null
     } catch (e) {
         console.log(e)
         return null
@@ -63,7 +79,8 @@ Scrapper.getType = async function(page){
 
 Scrapper.getTitle = async function(page){
     try {
-
+        const text = await page.$eval('div div a div.px-5 h1', element => element.innerText)
+        return text ? text.trim().toLowerCase() : null
     } catch (e) {
         console.log(e)
         return null
@@ -72,7 +89,11 @@ Scrapper.getTitle = async function(page){
 
 Scrapper.getQuestion = async function(page){
     try {
-
+        const text = await page.$$eval('div div a div.px-5 p.mb-1.text-md.text-ellipsis.line-clamp-6 p', elements => {
+            return elements.map(el => el.innerText);
+          });
+          //Falta ver aqui pra pegar a questão com multiplos P, ver com o gpt
+        return text ? text.trim().toLowerCase() : null
     } catch (e) {
         console.log(e)
         return null
@@ -90,7 +111,8 @@ Scrapper.getAnswer = async function(page){
 
 Scrapper.getDate = async function(page){
     try {
-
+        const date = await page.$eval('div div div.flex.justify-between.px-8', element => element.innerText)
+        return date ? stringToDate(date) : null
     } catch (e) {
         console.log(e)
         return new Date()
@@ -99,7 +121,8 @@ Scrapper.getDate = async function(page){
 
 Scrapper.getUserName = async function(page){
     try {
-
+        const name = await page.$eval('div div a div.flex.items-center.gap-3 h2', element => element.innerText)
+        return name ? name.trim().toLowerCase() : null
     } catch (e) {
         console.log(e)
         return "Sem user"
@@ -126,7 +149,8 @@ Scrapper.getClosed = async function(page){
 
 Scrapper.getHasImage = async function(page){
     try {
-
+        const image = await page.$('div div a div:nth-child(3)')
+        return image ? true : false
     } catch (e) {
         console.log(e)
         return null
@@ -150,6 +174,31 @@ Scrapper.new = async function (question) {
     } catch (e) {
         return e.message
     }
+}
+
+function stringToDate(dateStr) {
+    const parts = dateStr.split(" ");
+    
+    const months = {
+        "janeiro": 0,
+        "fevereiro": 1,
+        "março": 2,
+        "abril": 3,
+        "maio": 4,
+        "junho": 5,
+        "julho": 6,
+        "agosto": 7,
+        "setembro": 8,
+        "outubro": 9,
+        "novembro": 10,
+        "dezembro": 11
+    };
+    
+    const day = parseInt(parts[0], 10)
+    const month = months[parts[2].toLowerCase()]
+    const year = parseInt(parts[4], 10).toString().slice(0, 4)
+
+    return new Date(year, month, day)
 }
 
 module.exports = Scrapper
--- index.js
+++ index.js
@@ -8,17 +8,26 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-schedule.scheduleJob('*/2 * * * *', async () => {
+app.get('/teste', async (req, res) => {
     try {
-        const page = await PageControll.findOne({current: true})
-        const result = await Scrapper.run(page.last_page)
-        await PageControll.updateOne({_id: page._id}, {$set: {last_page: result}})
-        return console.log("Roudou até a página: " + result)
+        const result = await Scrapper.run(1)
+        return result.status(200).send(result)
     } catch (err) {
-        console.error(err)
-        return console.log('Erro interno: ', err.message)
+        return res.status(500).send(err)
     }
 })
+
+// schedule.scheduleJob('*/2 * * * *', async () => {
+//     try {
+//         const page = await PageControll.findOne({current: true})
+//         const result = await Scrapper.run(page.last_page)
+//         await PageControll.updateOne({_id: page._id}, {$set: {last_page: result}})
+//         return console.log("Roudou até a página: " + result)
+//     } catch (err) {
+//         console.error(err)
+//         return console.log('Erro interno: ', err.message)
+//     }
+// })
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
